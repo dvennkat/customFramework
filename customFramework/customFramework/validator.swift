9de8ec7dfe8bd33df9f4d95a6f87fddb70ab16ff
dvennkat <reddy.venky22@gmail.com>
2022-11-08 15:33:07 +0530

    Added first commit
    description added

---
+++ customFramework/customFramework/validator.swift
@@ -0,0 +1,20 @@
+//
+//  validator.swift
+//  customFramework
+//
+//  Created by Mobile Programming on 08/11/22.
+//
+
+import Foundation
+
+public struct validator {
+    public  static func printName( name :String) ->String
+    {
+        return "hello everyone \(name)"
+    }
+    
+    public static func sayHello(){
+        print("say hello to every one")
+    }
+    
+}
